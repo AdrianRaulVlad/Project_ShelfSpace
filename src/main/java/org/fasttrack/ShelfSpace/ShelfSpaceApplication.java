0df758d8c3dfa3ffb1518bc87c30b1e45152bccd
AdrianVlad <adrianraul.vlad@gmail.com>
2024-08-01 01:03:13 +0300

    #This is the first commit to create the spring project for ShelfSpace application

---
+++ src/main/java/org/fasttrack/ShelfSpace/ShelfSpaceApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrack.ShelfSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrack/ShelfSpace/ShelfSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrack.ShelfSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
