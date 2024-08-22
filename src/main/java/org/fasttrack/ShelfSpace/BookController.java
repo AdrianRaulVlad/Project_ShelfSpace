1cf23901e3a05bd847a66dacb4d2e74e1f1aad44
AdrianVlad <adrianraul.vlad@gmail.com>
2024-08-22 22:17:15 +0300

    #Add two more endpoints to have the CRUD actions

--- src/main/java/org/fasttrack/ShelfSpace/BookController.java
+++ src/main/java/org/fasttrack/ShelfSpace/BookController.java
@@ -1,10 +1,8 @@
 package org.fasttrack.ShelfSpace;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,23 @@ public class BookController {
     @GetMapping("/books/{id}")
     public Book getBookById(@PathVariable long id) {
         return bookService.getBookById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public Book deleteBookById(@PathVariable long id) {
+        return bookService.deleteBookEntry(id);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public Book updateBook(@PathVariable long id, @RequestBody Book book) {
+        if(id != book.getId()) {
+            throw new RuntimeException();
+        }
+        return bookService.updateBook(book);
     }
 }
